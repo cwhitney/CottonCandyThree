8bfb13fb345910369ed19cfb53d041a6e57929c0
Charlie Whitney <charlie@sharkbox.com>
2022-08-24 10:37:36 -0400

    Added in stirring with the bezel

--- CCT3/Assets/Scripts/CCTApp.cs
+++ CCT3/Assets/Scripts/CCTApp.cs
@@ -5,25 +5,43 @@ using UnityEngine;
 public class CCTApp : MonoBehaviour
 {
 	public AudioManager mAudioManager;
+	public InputManager mInputManager;
 
 	OscManager mOsc;
 	SugarSystem mSugarSystem = new SugarSystem();
 
+	public float maxWindSpeedHorz = 20.0f;
+	public float maxWindSpeedVert = 2.29f;
+
 	// Start is called before the first frame update
 	void Start() {
 		mSugarSystem.Setup();
+
+		mInputManager.OnChangeDir += OnStirChangeDir;
+		mInputManager.OnWindSpeedChanged += OnWindSpeedUpdate;
+
+		//Application.targetFrameRate = 30;
+	}
+
+	void OnStirChangeDir(int dir) {
+		//Debug.Log("Stir changed dir: " + dir);
+	}
+
+	void OnWindSpeedUpdate(float amt) {
+		//Debug.Log("Wind amount: " + amt);
+		mSugarSystem.SetWind(new Vector3(amt * maxWindSpeedHorz, maxWindSpeedVert, 0.0f));   // max wind speed 20.0
 	}
 
 	// Update is called once per frame
 	void Update() {
-		mSugarSystem.SetWind(new Vector3(Mathf.Sin(Time.fixedTime * 0.5f) * 20.0f, 2.29f, 0.0f));	// max wind speed 20.0
+		//mSugarSystem.SetWind(new Vector3(Mathf.Sin(Time.fixedTime * 0.5f) * maxWindSpeedHorz, maxWindSpeedVert, 0.0f));	// max wind speed 20.0
 		//mSugarSystem.SetWind(new Vector3(0, 2.29f, 0.0f));	// max wind speed 20.0
 		mSugarSystem.Update();
 
-		if (Input.GetKeyDown(KeyCode.S)) {
+		if (Input.GetKeyDown(KeyCode.G)) {
 			Debug.Log("Start spawning");
 			StartPressed();
-		} else if (Input.GetKeyDown(KeyCode.T)) {
+		} else if (Input.GetKeyDown(KeyCode.S)) {
 			Debug.Log("Stop spawning");
 
 			StopPressed();
--- CCT3/Assets/Scripts/InputManager.cs
+++ CCT3/Assets/Scripts/InputManager.cs
@@ -1,14 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 using CircularBuffer;
 
 public class InputManager : MonoBehaviour
 {
-	float mCircularDirection = 0f;
-	float mPitchBend = 0.0f;
+	public delegate void changeDirDelegate(int dir);
+	public changeDirDelegate OnChangeDir;
+
+	public delegate void changeWindDelegate(float speed);
+	public changeWindDelegate OnWindSpeedChanged;
+
+	public float windChangeAmount = 0.04f;
 
 	CircularBuffer<float> mAngleBuffer;
+	bool bIsCW = true;
+
+	const float mStirConfidenceThresh = 0.0f;
+	float mWindX = 0.0f;
+	float mCircularDirection = 0f;
+	float mPitchBend = 0.0f;
 
 	// Start is called before the first frame update
 	void Start() {
@@ -35,10 +47,18 @@ public class InputManager : MonoBehaviour
 		}
 
 		if (Input.touchCount > 0) {
-			Debug.Log("Got some touches");
 			Touch t = Input.GetTouch(0);
-			Debug.Log(t.position);
+			
+			float angleRads = Mathf.Atan2(t.position.y - h*0.5f, t.position.x - w*0.5f);
+			float ang = 180f / 3.1415926f * angleRads;
+			if (ang < 0) { ang += 360; }
+
+			mAngleBuffer.PushBack( ang );
+			CalculateDirection();
 		}
+
+		mWindX *= 0.999f;
+		OnWindSpeedChanged?.Invoke(mWindX);
 
 		// get the position and normalize from -1 to 1 with 0,0 in the center
 	}
@@ -48,13 +68,9 @@ public class InputManager : MonoBehaviour
 		int angleMovedPos = 0;
 		int angleMovedNeg = 0;
 		int numSamples = 60;
-		/*
+		
 		mCircularDirection = 0;
 		for (int i = 0; i < numSamples; i++) {
-			
-
-			float hh = mAngleBuffer[2];
-
 			float angle = mAngleBuffer.Back(i);
 			float angleP = mAngleBuffer.Back(i + 1);
 			if (angle > 340 && angleP < 20) {        // ccw
@@ -65,56 +81,65 @@ public class InputManager : MonoBehaviour
 				++angleMovedPos;
 			} else {
 				mCircularDirection += (angle - angleP);
-				(angle > angleP) ? (++angleMovedPos) : (++angleMovedNeg);
+				if (angle > angleP) {
+					++angleMovedPos;
+				} else { 
+					++angleMovedNeg;
+ 				}
 			}
 		}
 
-		float confidence = 0.0;
+		float confidence = 0.0f;
 
 		if (mCircularDirection > 0.0) {
 			confidence = (float)angleMovedPos / (float)numSamples;
 
-			if (confidence > mModel->mStirConfidenceThresh) {
+			if (confidence > mStirConfidenceThresh) {
 				if (!bIsCW) {
-					mPitchBend = 0.0;
-					signalChangeDir.emit(1);
+					//mPitchBend = 0.0;
+					OnChangeDir?.Invoke(1);
 				} else {
-					mPitchBend = math<float>::clamp(mPitchBend + 0.02);
-					mModel->mWindX = math<float>::clamp(mModel->mWindX + 0.04, -1.0, 1.0);     // wind x
+					mPitchBend = Mathf.Clamp01(mPitchBend + 0.02f);
+					mWindX = Mathf.Clamp(mWindX + windChangeAmount * Time.deltaTime, -1.0f, 1.0f);     // wind x
 				}
 				bIsCW = true;
 			} else {
-				mPitchBend *= 0.93;
-				mModel->mWindX *= 0.999;
+				//mPitchBend *= 0.93;
+				//mWindX *= 0.999f;
 			}
+
+			//OnWindSpeedChanged?.Invoke(mWindX);
 		} else if (mCircularDirection < 0.0) {
 			confidence = (float)angleMovedNeg / (float)numSamples;
 
-			if (confidence > mModel->mStirConfidenceThresh) {
+			if (confidence > mStirConfidenceThresh) {
 				if (bIsCW) {
-					mPitchBend = 0.0;
-					signalChangeDir.emit(-1);
+					//mPitchBend = 0.0;
+					//signalChangeDir.emit(-1);
+					OnChangeDir?.Invoke(-1);
 				} else {
-					mPitchBend = math<float>::clamp(mPitchBend + 0.02);
-					mModel->mWindX = math<float>::clamp(mModel->mWindX - 0.04, -1.0, 1.0);     // wind x
+					//mPitchBend = math<float>::clamp(mPitchBend + 0.02);
+					mWindX = Mathf.Clamp(mWindX - windChangeAmount * Time.deltaTime, -1.0f, 1.0f);     // wind x
 				}
 				bIsCW = false;
 			} else {
-				mPitchBend *= 0.93;
-				mModel->mWindX *= 0.999;
+				//mPitchBend *= 0.93;
+				//mWindX *= 0.999f;
 			}
+
+			//OnWindSpeedChanged?.Invoke(mWindX);
 		}
 
-		if (mPitchBend < 0.001) {
-			mPitchBend = 0.0;
-		}
+		//if (mPitchBend < 0.001) {
+		//	mPitchBend = 0.0;
+		//}
 
-		if (bIsCW) {
-			signalWandSpeedDir.emit(mPitchBend);
-		} else {
-			signalWandSpeedDir.emit(-mPitchBend);
-		}
-		*/
+		//if (bIsCW) {
+		//	signalWandSpeedDir.emit(mPitchBend);
+		//} else {
+		//	signalWandSpeedDir.emit(-mPitchBend);
+		//}
+		
 		return 0;
 	}
 }
